9bd72481e2084695b224c19478c9cb28e0940873
Andrew Waller <andrewhwaller@gmail.com>
2019-04-18 19:36:08 +0000

    Automatically backed up by Learn

--- db/migrate/20190418193413_create_movies.rb
+++ db/migrate/20190418193413_create_movies.rb
@@ -1,4 +1,12 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
+    create_columns do |c|
+      c.string :title
+      c.integer :release_date
+      c.string :director
+      c.string :lead
+      c.boolean :in_theaters
+
+      t.timestamps null: false
   end
 end
